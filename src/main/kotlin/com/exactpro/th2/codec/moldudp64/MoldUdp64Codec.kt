3a60a7c4407ed51dc45a6a131971f7ea80e06e9a
kseniaavilova <89018971+kseniaavilova@users.noreply.github.com>
2021-08-26 00:35:47 +0300

    All Test

--- src/main/kotlin/com/exactpro/th2/codec/moldudp64/MoldUdp64Codec.kt
+++ src/main/kotlin/com/exactpro/th2/codec/moldudp64/MoldUdp64Codec.kt
@@ -26,10 +26,12 @@ import com.google.protobuf.ByteString
 import com.google.protobuf.Timestamp
 import java.nio.ByteBuffer
 import java.nio.charset.Charset
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCodec {
 
-    private fun convertToBytes(value: Int): ByteArray? {
+    private fun convertToBytes(value: Int): ByteArray {
         val result = ByteArray(2)
         result[0] = (value ushr 8 and 0xFF).toByte()
         result[1] = (value and 0xFF).toByte()
@@ -56,10 +58,10 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
 
         // encode header and payload here
         var messageBody: ByteString
-        var index: Int = 0
-        var propertiesMap: Map<String,String>
-        var timestamp: Timestamp
-        var messageId: MessageID
+        var index = 0
+        val propertiesMap: Map<String, String>
+        val timestamp: Timestamp
+        val messageId: MessageID
         if (header == null) {
             messageBody = ByteString.copyFrom(" ".repeat(10).toByteArray())
             messageBody = messageBody.concat(ByteString.copyFrom(ByteArray(8)))
@@ -69,24 +71,90 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
             messageId = payload[0].metadata.id
 
             for (msg in payload) {
-                println(msg.body.size())
                 messageBody = messageBody.concat(ByteString.copyFrom(convertToBytes(msg.body.size())))
                 messageBody = messageBody.concat(msg.body)
             }
 
         } else {
-            val sessionField = header.getString(SESSION_FIELD)
+
             val sequenceField = header.getLong(SEQUENCE_FIELD)
-            val countMessage = header.getInt(COUNT_FIELD)
+                ?: throw IllegalArgumentException(
+                    "Cannot fit sequence number into 8 bytes: " + header.getString(
+                        SEQUENCE_FIELD
+                    )
+                )
+            if (sequenceField < 0) {
+                throw IllegalArgumentException("Negative sequence number: $sequenceField")
+
+            }
             val lengthsField = header.getList(LENGTHS_FIELD)
+            val countMessage = header.getInt(COUNT_FIELD)
+            if (countMessage != null) {
+                if (countMessage > 65535) {
+                    throw IllegalArgumentException("Cannot fit message count into 2 bytes: $countMessage")
+                }
+                if (countMessage < 0) {
+                    throw IllegalArgumentException("Negative message count: $countMessage")
+                }
+            }
+            if (countMessage != 65535 && header.metadata.id.direction != Direction.SECOND) {
+
+
+                if (lengthsField != null) {
+                    if (countMessage != null) {
+                        if (countMessage < lengthsField.size) {
+                            throw IllegalArgumentException("Invalid amount of message lengths: " + lengthsField.size + " (expected: " + countMessage + ")")
+                        }
+                    }
+                }
+
+
+
+                if (countMessage != payload.size) {
+                    throw IllegalArgumentException("Invalid message count: " + countMessage + " (expected: " + payload.size + ")")
+                }
+
+
+                if (lengthsField != null) {
+                    for (i in lengthsField.indices) {
+                        val num = lengthsField[i].getInt()
+                        if (num != null) {
+                            if (num > 65535) {
+                                throw IllegalArgumentException("Cannot fit length of message 1 into 2 bytes: $num")
+                            }
+                            if (num < 0) {
+                                throw IllegalArgumentException("Negative length of message " + (i + 1) + ": " + num)
+                            }
+                            val messageSize: Int = payload[i].body.size()
+                            if (num != messageSize) {
+                                throw java.lang.IllegalArgumentException("Mismatching length of message " + (i + 1) + ": " + num + " (expected: " + messageSize + ")")
+                            }
+                        }
+                    }
+                    for (i in payload.indices) {
+                        val messageSize: Int = payload[i].body.size()
+                        if (messageSize > 65535) {
+                            throw java.lang.IllegalArgumentException("Actual length of message " + (i + 1) + " is too big: " + messageSize + " (expected at most: 65535)")
+                        }
+                    }
+                }
+            }
+
+            val sessionField = header.getString(SESSION_FIELD)
+            if (sessionField != null) {
+                if (sessionField.toByteArray().size > 10) {
+                    throw IllegalArgumentException("Cannot fit session alias into 10 bytes: $sessionField")
+                }
+            }
+
             propertiesMap = header.metadata.propertiesMap
             timestamp = header.metadata.timestamp
             messageId = header.metadata.id
             messageBody = ByteString.copyFrom(sessionField, Charset.defaultCharset())
-            messageBody = messageBody.concat(ByteString.copyFrom(sequenceField?.let { Longs.toByteArray(it) }))
+            messageBody = messageBody.concat(ByteString.copyFrom(sequenceField.let { Longs.toByteArray(it) }))
             messageBody = messageBody.concat(ByteString.copyFrom(countMessage?.let { convertToBytes(it) }))
 
-            if (!payload.isEmpty()) {
+            if (payload.isNotEmpty()) {
                 for (msg in payload) {
                     messageBody = messageBody.concat(
                         ByteString.copyFrom(lengthsField?.get(index)?.getInt()
@@ -98,20 +166,21 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
             }
 
         }
-            builder.addMessages(
-                AnyMessage.newBuilder().setRawMessage(
-                    RawMessage.newBuilder()
-                        .setBody(messageBody)
-                        .setMetadata(
-                            RawMessageMetadata.newBuilder()
-                                .putAllProperties(propertiesMap)
-                                .setTimestamp(timestamp)
-                                .setId(MessageID.newBuilder(messageId).build())
-                        )
-                )
+        builder.addMessages(
+            AnyMessage.newBuilder().setRawMessage(
+                RawMessage.newBuilder()
+                    .setBody(messageBody)
+                    .setMetadata(
+                        RawMessageMetadata.newBuilder()
+                            .putAllProperties(propertiesMap)
+                            .setTimestamp(timestamp)
+                            .setId(MessageID.newBuilder(messageId).build())
+                    )
             )
+        )
         return builder.build()
     }
+
 
     override fun decode(messageGroup: MessageGroup): MessageGroup {
         val messages = messageGroup.messagesList
@@ -127,6 +196,9 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
         require(protocol.isEmpty() || protocol == PROTOCOL) { "Unexpected protocol: $protocol (expected: $PROTOCOL)" }
 
         val builder = MessageGroup.newBuilder()
+        if (message.body.size()<20) {
+            throw  IllegalArgumentException("Not enough bytes to decode packet header from: " + message.body.size() + " (expected at least: 20)")
+        }
 
         // decode message's body here
         val sessionField = String(message.body.substring(0, 10).toByteArray())
@@ -141,17 +213,38 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
                 lengthsField.add(ByteBuffer.wrap(message.body.substring(index, index + 2).toByteArray()).short)
                 index += ByteBuffer.wrap(message.body.substring(index, index + 2).toByteArray()).short + 2
             }
-            var firstIndex: Int = 22
-            var secondIndex: Int = lengthsField.get(0).toInt() + firstIndex
+            var firstIndex = 22
+            var secondIndex: Int = lengthsField[0].toInt() + firstIndex
             index = 1
             while (index != countField.toInt()) {
                 messagesField.add(message.body.substring(firstIndex, secondIndex))
                 firstIndex = secondIndex + 2
-                secondIndex = firstIndex + lengthsField.get(index)
+                secondIndex = firstIndex + lengthsField[index]
                 index++
+            }
+            if (secondIndex>message.body.size()) {
+                throw IllegalArgumentException("Not enough bytes to decode message "+ (messagesField.size+1) +" from: "+(message.body.size()-firstIndex) + " (expected: " + lengthsField[index-1] + ")")
             }
             messagesField.add(message.body.substring(firstIndex, secondIndex))
         }
+
+        if (countField.toInt() == 65535 || message.metadata.id.direction == Direction.SECOND) {
+
+        } else {
+            if (countField.toInt() > 0 && message.body.size() == 20) {
+                throw IllegalArgumentException("Not enough bytes to decode " + countField.toInt() + " messages from: 0 (expected at least: " + countField.toInt() * 2 + ")")
+            }
+
+            if (message.body.size() > 20) {
+                var messageSize = 0
+                for (integer in lengthsField) {
+                    messageSize += integer + 2
+                }
+                check(message.body.size() -  messageSize <= 20) { "Decoded all messages but still have " + (message.body.size() -  messageSize - 20) + " bytes to decode" }
+            }
+        }
+
+
         builder.addMessages(
             AnyMessage.newBuilder().setMessage(
                 Message.newBuilder().addFields(
@@ -170,7 +263,7 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
                     .build()
             )
         )
-        if (!messagesField.isEmpty()) {
+        if (messagesField.isNotEmpty()) {
             var k = 1
             for (msg in messagesField) {
                 builder.addMessages(
@@ -200,4 +293,5 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
         const val LENGTHS_FIELD = "MessageLengths"
     }
 }
+
 
