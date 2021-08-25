f852b880fdfb425ed0d47fb826211d502c60244c
kseniaavilova <89018971+kseniaavilova@users.noreply.github.com>
2021-08-25 20:54:49 +0300

    Positive Test

--- src/main/kotlin/com/exactpro/th2/codec/moldudp64/MoldUdp64Codec.kt
+++ src/main/kotlin/com/exactpro/th2/codec/moldudp64/MoldUdp64Codec.kt
@@ -19,12 +19,23 @@ package com.exactpro.th2.codec.moldudp64
 import com.exactpro.th2.codec.api.IPipelineCodec
 import com.exactpro.th2.codec.moldudp64.MoldUdp64CodecFactory.Companion.PROTOCOL
 import com.exactpro.th2.common.grpc.*
-import com.exactpro.th2.common.message.addFields
-import com.exactpro.th2.common.message.messageType
+import com.exactpro.th2.common.message.*
+import com.exactpro.th2.common.value.getInt
+import com.google.common.primitives.Longs
 import com.google.protobuf.ByteString
+import com.google.protobuf.Timestamp
 import java.nio.ByteBuffer
+import java.nio.charset.Charset
 
 class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCodec {
+
+    private fun convertToBytes(value: Int): ByteArray? {
+        val result = ByteArray(2)
+        result[0] = (value ushr 8 and 0xFF).toByte()
+        result[1] = (value and 0xFF).toByte()
+        return result
+    }
+
     override fun encode(messageGroup: MessageGroup): MessageGroup {
         val messages = messageGroup.messagesList
 
@@ -44,7 +55,61 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
         val builder = MessageGroup.newBuilder()
 
         // encode header and payload here
+        var messageBody: ByteString
+        var index: Int = 0
+        var propertiesMap: Map<String,String>
+        var timestamp: Timestamp
+        var messageId: MessageID
+        if (header == null) {
+            messageBody = ByteString.copyFrom(" ".repeat(10).toByteArray())
+            messageBody = messageBody.concat(ByteString.copyFrom(ByteArray(8)))
+            messageBody = messageBody.concat(ByteString.copyFrom(convertToBytes(payload.size)))
+            propertiesMap = payload[0].metadata.propertiesMap
+            timestamp = payload[0].metadata.timestamp
+            messageId = payload[0].metadata.id
 
+            for (msg in payload) {
+                println(msg.body.size())
+                messageBody = messageBody.concat(ByteString.copyFrom(convertToBytes(msg.body.size())))
+                messageBody = messageBody.concat(msg.body)
+            }
+
+        } else {
+            val sessionField = header.getString(SESSION_FIELD)
+            val sequenceField = header.getLong(SEQUENCE_FIELD)
+            val countMessage = header.getInt(COUNT_FIELD)
+            val lengthsField = header.getList(LENGTHS_FIELD)
+            propertiesMap = header.metadata.propertiesMap
+            timestamp = header.metadata.timestamp
+            messageId = header.metadata.id
+            messageBody = ByteString.copyFrom(sessionField, Charset.defaultCharset())
+            messageBody = messageBody.concat(ByteString.copyFrom(sequenceField?.let { Longs.toByteArray(it) }))
+            messageBody = messageBody.concat(ByteString.copyFrom(countMessage?.let { convertToBytes(it) }))
+
+            if (!payload.isEmpty()) {
+                for (msg in payload) {
+                    messageBody = messageBody.concat(
+                        ByteString.copyFrom(lengthsField?.get(index)?.getInt()
+                            ?.let { convertToBytes(it) })
+                    )
+                    messageBody = messageBody.concat(msg.body)
+                    index++
+                }
+            }
+
+        }
+            builder.addMessages(
+                AnyMessage.newBuilder().setRawMessage(
+                    RawMessage.newBuilder()
+                        .setBody(messageBody)
+                        .setMetadata(
+                            RawMessageMetadata.newBuilder()
+                                .putAllProperties(propertiesMap)
+                                .setTimestamp(timestamp)
+                                .setId(MessageID.newBuilder(messageId).build())
+                        )
+                )
+            )
         return builder.build()
     }
 
@@ -71,19 +136,19 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
         val messagesField = ArrayList<ByteString>()
 
         if (message.body.size() > 20) {
-            var k = 20
+            var index = 20
             while (lengthsField.size != countField.toInt()) {
-                lengthsField.add(ByteBuffer.wrap(message.body.substring(k, k + 2).toByteArray()).short)
-                k += ByteBuffer.wrap(message.body.substring(k, k + 2).toByteArray()).short + 2
+                lengthsField.add(ByteBuffer.wrap(message.body.substring(index, index + 2).toByteArray()).short)
+                index += ByteBuffer.wrap(message.body.substring(index, index + 2).toByteArray()).short + 2
             }
             var firstIndex: Int = 22
             var secondIndex: Int = lengthsField.get(0).toInt() + firstIndex
-            k = 1
-            while (k != countField.toInt()) {
+            index = 1
+            while (index != countField.toInt()) {
                 messagesField.add(message.body.substring(firstIndex, secondIndex))
                 firstIndex = secondIndex + 2
-                secondIndex = firstIndex + lengthsField.get(k)
-                k++
+                secondIndex = firstIndex + lengthsField.get(index)
+                index++
             }
             messagesField.add(message.body.substring(firstIndex, secondIndex))
         }
@@ -123,12 +188,7 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
                 k++
             }
         }
-        println(sessionField)
-        println(sequenceField)
-        println(countField)
-        println(lengthsField)
-        println(messagesField)
-        println(builder.build())
+
         return builder.build()
     }
 
@@ -140,3 +200,4 @@ class MoldUdp64Codec(private val settings: MoldUdp64CodecSettings) : IPipelineCo
         const val LENGTHS_FIELD = "MessageLengths"
     }
 }
+
